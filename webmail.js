8c63e4f624eee0f827c029b497c24202e3eae4ca
Julien Du Crest <v1jducr@godfrey.inf.ed.ac.uk>
2018-06-18 11:29:54 +0100

    my very first commit !

---
+++ webmail.js
@@ -0,0 +1,20 @@
+var element = document.getElementById("toplogo");
+element.parentNode.removeChild(element);
+var element = document.getElementById("rcmbtn120");
+element.parentNode.removeChild(element);
+var element = document.getElementById("rcmbtn125");
+element.parentNode.removeChild(element);
+
+var element = document.getElementById("rcmbtn109");
+element.parentNode.removeChild(element);
+
+var element = document.getElementById("markmessagemenulink");
+element.parentNode.removeChild(element);
+var element = document.getElementById("messagemenulink");
+element.parentNode.removeChild(element);
+var element = document.getElementById("searchfilter");
+element.parentNode.removeChild(element);
+var element = document.getElementById("messagelistfooter");
+element.parentNode.removeChild(element);
+var element = document.getElementById("countcontrols");
+element.parentNode.removeChild(element);
